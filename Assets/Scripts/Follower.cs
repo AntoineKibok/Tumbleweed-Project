2601f19465e6a547564f55f83a5c77ba0a96c5ae
AntoineKibok <73173590+AntoineKibok@users.noreply.github.com>
2021-10-10 17:32:07 +0200

    Base et ajout controller et camera

---
+++ Assets/Scripts/Follower.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follower : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 lookDecal;
+    public Vector3 followDecal;
+    public Rigidbody rb;
+    public float speedDecal = 1;
+    public float dist = 5;
+    public float height = 2;
+    
+    void Update()
+    {
+        transform.LookAt(target.TransformPoint(lookDecal));
+     
+        transform.position = target.position - (target.position - transform.position).normalized * dist;
+    }
+}
---
+++ Assets/Scripts/TumbelWeedController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TumbelWeedController : MonoBehaviour
+{
+    public Vector3 direction;
+    public Transform camTransfom;
+    public Rigidbody rb;
+    public Transform helper;
+    public float moveSpeed = 100;
+
+    void Update()
+    {
+        direction = transform.position - camTransfom.position;
+        direction.y = 0;
+        direction = direction.normalized;
+
+        helper.position = transform.position;
+        helper.LookAt(transform.position + direction);
+
+        rb.AddForce(Input.GetAxis("Horizontal") * moveSpeed * helper.right +
+                      Input.GetAxis("Vertical") * moveSpeed * helper.forward);
+    }
+}
