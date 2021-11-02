aa761a14353b67a5054d318f62b2376c08352ba6
AntoineKibok <73173590+AntoineKibok@users.noreply.github.com>
2021-11-02 20:49:59 +0100

    Déplacements fonctionnels mais pas polish

--- Assets/Scripts/TumbleController.cs
+++ Assets/Scripts/TumbleController.cs
@@ -4,18 +4,17 @@ using UnityEngine;
 using UnityEngine.UI;
 using System;
 
-public class NewController : MonoBehaviour
+public class TumbleController : MonoBehaviour
 {
     [Header("GameObjects")]
     public Transform camTransfom;
     private Rigidbody rb;
     public Transform helper;
     private PhysicMaterial physicMaterial;
-    private SphereCollider Scollider;
+    private SphereCollider sCollider;
 
     [Header("Valeurs")]
     public Vector3 direction;
-    public float moveSpeed = 100;
     private float xPosition;
     private float yPosition;
     private int zSpeed;
@@ -23,49 +22,44 @@ public class NewController : MonoBehaviour
     public int scale = 20;
     public int ignorePercent = 5;
     private int ignoreSize;
-    public int maxSpeed = 60;
 
-    public int speedFactor = 0;
-    public float brakeFactor = 0.2f;
+
+    public int maxSpeed = 60;
+    public float speedFactor = 0.7f;
+    public float brakeFactor = 1.2f;
+    public float rotateFactor = 1f;
 
     [Header("Debug")]
-    public Text vValue;
-    public Text yValue;
+    public bool showDebug;
+    public Text line1;
+    public Text line2;
+    public Text line3;
     private bool ignoreStatut = true;
 
     private void Start()
     {
         //Recupération du rigidbody
         rb = gameObject.GetComponent<Rigidbody>();
-        Scollider = gameObject.GetComponent<SphereCollider>();
-        physicMaterial = Scollider.material;
         //Génère la valeur de la zone a ignorer
         ignoreSize = scale / (ignorePercent);
+
+        //Masque le débug
+        if (!showDebug)
+        {
+            line1.text = "";
+            line2.text = "";
+            line3.text = "";
+        }
     }
 
     void Update()
     {
-
         findCursorPosition();
-
-        direction = transform.position - camTransfom.position;
-        //direction.y = +yPosition/70;
-        direction.y = 0;
-        //direction.z = -xSpeed / 50; 
-        direction = direction.normalized;
-
-        helper.position = transform.position;
-        helper.LookAt(transform.position + direction);
-
-        //rb.AddForce(Input.GetAxis("Horizontal") * moveSpeed * helper.right +
-        //              Input.GetAxis("Vertical") * moveSpeed * helper.forward);
-
-        //camTransfom.position = new Vector3(camTransfom.position.x, camTransfom.position.y, camTransfom.position.z + (xSpeed/50));
-        //rb.AddForce(xSpeed/5, 0 ,0);
-
+        dirCamera();
         setSpeed();
         checkMaxSpeed();
-
+        setDirection();
+        debug();
     }
 
     void findCursorPosition()
@@ -79,7 +73,8 @@ public class NewController : MonoBehaviour
         xSpeed = (int)Math.Ceiling(yPosition / (Screen.height / 2) * scale);
 
         //Protège des sorties d'écran
-        if (zSpeed > scale) {
+        if (zSpeed > scale) 
+        {
             zSpeed = scale;
         }
         if (zSpeed < -scale)
@@ -94,10 +89,16 @@ public class NewController : MonoBehaviour
         {
             xSpeed = -scale;
         }
+    }
 
-        //Debug
-        vValue.text = rb.velocity.magnitude.ToString() + " - " + ignoreSize + "  " + ignoreStatut + "  " + physicMaterial.dynamicFriction;
-        yValue.text = "x: " + xSpeed +" z: " + zSpeed;
+    void dirCamera()
+    {
+        direction = transform.position - camTransfom.position;
+        direction.y = 0;
+        direction = direction.normalized;
+
+        helper.position = transform.position;
+        helper.LookAt(transform.position + direction);
     }
 
     void setSpeed()
@@ -107,11 +108,9 @@ public class NewController : MonoBehaviour
         if (xSpeed > ignoreSize)
         {
             {
-                if (ignoreStatut == true)
-                {
-                    physicMaterial.dynamicFriction = 0.6f;
-                }
-                rb.AddForce((xSpeed - ignoreSize) * speedFactor, 0, 0, ForceMode.Acceleration);
+                Vector3 dir = camTransfom.forward;
+                rb.AddForce((xSpeed - ignoreSize) * speedFactor * dir, ForceMode.Acceleration);
+
                 ignoreStatut = false;
             }
         }
@@ -120,19 +119,8 @@ public class NewController : MonoBehaviour
         {
             if (xSpeed > 0 && rb.velocity.magnitude > 0) 
             {
-                rb.velocity = new Vector3(rb.velocity.x - (1 * Time.deltaTime), 0, 0);
+                rb.velocity = new Vector3(rb.velocity.x - (brakeFactor * Time.deltaTime), 0, 0);
             }
-            /*
-            if (ignoreStatut == false)
-            {
-                rb.AddForce(-rb.velocity.x * brakeFactor, 0, 0, ForceMode.Impulse);
-                physicMaterial.dynamicFriction = 2;
-            }
-            if(rb.velocity.magnitude < 3)
-            {
-
-            }
-            //rb.AddForce(-rb.velocity.x * brakeFactor, 0, 0, ForceMode.Acceleration);*/
             ignoreStatut = true; 
         }
     }
@@ -144,4 +132,24 @@ public class NewController : MonoBehaviour
             rb.velocity = rb.velocity.normalized * maxSpeed;
         }
     }
+
+    void setDirection()
+    {
+        if (Math.Abs(zSpeed) > ignoreSize)
+        {
+            Vector3 dir = camTransfom.right;
+            rb.AddForce(zSpeed * rotateFactor * dir, ForceMode.Acceleration);
+        }
+    }
+
+    void debug()
+    {
+        if (showDebug)
+        {
+            line1.text = "Vitesse: " + rb.velocity.magnitude.ToString() + "  - Zone ignorée " + ignoreSize + ": " + ignoreStatut;
+            line2.text = "x: " + xSpeed + " * " + speedFactor +" = " + xSpeed*speedFactor;
+            line3.text = "z: " + zSpeed + " * " + rotateFactor + " = " + zSpeed * rotateFactor;
+        }
+    }
+
 }
