d15b65a8602cef744c2ea6a299502a96934e27de
AntoineKibok <antoinespam@protonmail.com>
2021-12-03 12:24:54 +0100

    Ajout du son

---
+++ Assets/Scripts/Sound/SoundFromObject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SoundFromObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public List<AudioClip> audioClips;
+    private AudioSource source;
+
+    void Start()
+    {
+         source = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        source.clip = audioClips[Random.Range(0, audioClips.Count)];
+        source.Play();
+    }
+}
