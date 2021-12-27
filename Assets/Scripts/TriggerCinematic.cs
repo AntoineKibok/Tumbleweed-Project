90a4f4848d4e8ab72df794b1aaff7906dfda40a6
Antoine <antoinespam@protonmail.com>
2021-12-27 16:28:37 +0100

    Début cinematiques

---
+++ Assets/Scripts/TriggerCinematic.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerCinematic : MonoBehaviour
+{
+    public GameObject highlight;
+    public PlayableAnimator 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
