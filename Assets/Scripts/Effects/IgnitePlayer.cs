59c04d1d5f7d01e50b02b27a4033c542c202f74a
AntoineKibok <antoinespam@protonmail.com>
2022-01-06 23:02:17 +0100

    v 0.2.a - Début feu fonctionnel

--- Assets/Scripts/Effects/IgnitePlayer.cs
+++ Assets/Scripts/Effects/IgnitePlayer.cs
@@ -8,9 +8,7 @@ public class IgnitePlayer : MonoBehaviour {
     public float cooldown;              //How often this cooldown may be used
     public float timer;                 //Time left on timer, can be used at 0
     public float length = 60;
-    public SphereCollider collider;
-    
-    public GameObject fireOnPlayer;
+    public GameObject fire;
     
     private void OnTriggerEnter(Collider other)
     {
@@ -22,10 +20,27 @@ public class IgnitePlayer : MonoBehaviour {
             Ignite();
         }
     }
+    
+    private void CollideDetection()
+    {
+        RaycastHit hit;
+        Vector3 p1 = transform.position;
+
+        // Cast a sphere wrapping character controller 10 meters forward
+        // to see if it is about to hit anything.
+        if (Physics.SphereCast(p1, 1, transform.forward, out hit, 10))
+        {
+            if (hit.transform.tag == "Fire")
+            {
+                Ignite();
+            }
+        }
+    }
 
     private void Ignite()
     {
-        fireOnPlayer.SetActive(true);
+        Debug.Log("Faya");
+        fire.SetActive(true);
         timer = length;
         active = true;
     }
@@ -34,14 +49,24 @@ public class IgnitePlayer : MonoBehaviour {
     {
         active = false;
         Debug.Log("Plus faya");
-        fireOnPlayer.SetActive(false);
+        fire.SetActive(false);
+    }
+
+    private void CorrectScale()
+    {
+        float scale = Mathf.InverseLerp(0, length, timer);
+        fire.transform.localScale = new Vector3(scale,scale,scale);
     }
     
     void Update () {
+        
+        CollideDetection();
+        
         if (active)
         {
+            CorrectScale();
             timer -= Time.deltaTime;    //Subtract the time since last frame from the timer.
-            Debug.Log(timer);
+            //Debug.Log(timer);
 
         }
 
