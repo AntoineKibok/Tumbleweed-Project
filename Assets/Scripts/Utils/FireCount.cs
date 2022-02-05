a45c4b22986063bd3b07da9a066fd46c7f767842
AntoineKibok <antoinespam@protonmail.com>
2022-02-05 18:13:01 +0100

    Ajout musique bundem

--- Assets/Scripts/Utils/FireCount.cs
+++ Assets/Scripts/Utils/FireCount.cs
@@ -11,14 +11,17 @@ public class FireCount : MonoBehaviour
     public void AddFire()
     {
         fireCount++;
+
+        if (fireCount == 2)
+        {
+            GetComponent<AudioSource>().Play();
+        }
+        
         if (fireCount >= totalCount)
         {
+            GetComponent<AudioSource>().Stop();
             burntField.GetComponent<TriggerCinematic>().manualStart=true;
         } 
     }
     
-    public int getFireCount()
-    {
-        return fireCount;
-    }
 }
